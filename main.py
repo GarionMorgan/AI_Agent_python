0767977586087d8ae4b4ed5194934ef3e29c3343
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-07-29 10:30:52 -0500

    Added multiple messages to be used for a session

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 #loads information from the dotenv file
 load_dotenv()
 #accesses the api key
 api_key = os.environ.get("GEMINI_API_KEY")
 #operating the gemini client with the api key
 client = genai.Client(api_key=api_key)
+
+
 
 
 def main():
@@ -18,9 +21,13 @@ def main():
         sys.exit(1)
     #saves user prompt from the CLI
     user_prompt = sys.argv[1]
-
+    #keeping track of mulitple messages from the user
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+    #generating the response from the LLM
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=user_prompt
+        model='gemini-2.0-flash-001', contents=messages,
     )
 
     print(response.text)
