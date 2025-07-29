dacd5d791f1fa6788dea1b69be10ddb81a95be5c
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-07-29 10:18:46 -0500

    Updated user prompt to be added to the CLI

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-
+#loads information from the dotenv file
 load_dotenv()
-
+#accesses the api key
 api_key = os.environ.get("GEMINI_API_KEY")
-
+#operating the gemini client with the api key
 client = genai.Client(api_key=api_key)
+
 
 def main():
     print("Hello from ai-agent-project!")
+    #calling the sys import to grab user input from the CLI
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <User request>")
+        sys.exit(1)
+    #saves user prompt from the CLI
+    user_prompt = sys.argv[1]
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=user_prompt
     )
 
     print(response.text)
