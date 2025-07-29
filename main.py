84a4fa444c5448c1e662e6bf6f855e3fe18fab4b
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-07-29 12:11:53 -0500

    Added verbose functionality to CLI

--- main.py
+++ main.py
@@ -3,38 +3,59 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-#loads information from the dotenv file
-load_dotenv()
-#accesses the api key
-api_key = os.environ.get("GEMINI_API_KEY")
-#operating the gemini client with the api key
-client = genai.Client(api_key=api_key)
+
 
 
 
 
 def main():
+    #loads information from the dotenv file
+    load_dotenv()
+    #accesses the api key
+    api_key = os.environ.get("GEMINI_API_KEY")
+    #operating the gemini client with the api key
+    client = genai.Client(api_key=api_key)
+
+
     print("Hello from ai-agent-project!")
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
     #calling the sys import to grab user input from the CLI
-    if len(sys.argv) < 2:
-        print(f"Usage: python3 main.py <User request>")
+    if not args:
+        print(f"AI Code Assistant")
+        print(f'\nUsage: python main.py "your prompt here" [--verbose]')
+        print(f'Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
     #saves user prompt from the CLI
-    user_prompt = sys.argv[1]
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
     #keeping track of mulitple messages from the user
     messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
+    
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
+       
     #generating the response from the LLM
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', contents=messages,
     )
-
+    if verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        candidates_tokens = response.usage_metadata.candidates_token_count
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {candidates_tokens}")
+    print("Response:")
     print(response.text)
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    candidates_tokens = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {candidates_tokens}")
+    
 
 
 if __name__ == "__main__":
